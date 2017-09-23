3551f989446197a3607ee4f4cc34d8014e304834
Kez123 <32081576+Kez123@users.noreply.github.com>
2017-09-23 12:03:42 +0800

    final
    updated version

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+//Load the DoM
+$(document).ready(function(){
+
+  //Log...
+  console.log('Ready!');
+
+  //Click the hamburger icon('
+  $('.toggle-mobile-menu').click(function(){
+
+    //show the mobile menu
+    $('#mobile-menu').toggleClass('mobile-menu-visible');
+
+  });
+
+  //close the mobile menu
+  //clicking in the cross icon
+
+
+});
